549255d186c2f4b617db69c10b3e397ac489b3b1
ram1k <zo0zo0@mail.ru>
2021-12-09 00:39:57 +0300

    6th lesson - Декораторы и продолжение работы с сетью

---
+++ 6th lesson - Декораторы и продолжение работы с сетью/decos.py
@@ -0,0 +1,34 @@
+# Decos
+
+from functools import wraps
+import inspect
+import logging
+
+class Log():
+    def __init__(self, logger=None):
+        self.logger = logger
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            parent_func_name = inspect.currentframe().f_back.f_code.co_name
+            module_name = inspect.currentframe().f_back.f_code.co_filename.split("/")[-1]
+            if not self.logger:
+                self.logger = logging.getLogger(module_name.split('.')[0])
+            self.logger.addFilter(RenameFilter(module_name))
+            self.logger.debug(f' {func.__name__} вызвана из функции {parent_func_name} '
+                              f'в модуле {module_name} с аргументами: {args}; {kwargs}')
+            result = func(*args, **kwargs)
+            return result
+
+        return wrapper
+
+
+class RenameFilter(logging.Filter):
+    def __init__(self, filename):
+        super().__init__()
+        self.filename = filename
+
+    def filter(self, record):
+        record.filename = self.filename
+        return True
